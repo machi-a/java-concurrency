2278439f7b07d3f72b1c1c03ed7a21b13e789b5f
machi-a <laijyeyi@gmail.com>
2022-03-13 16:34:38 +0800

    added buffer

--- FoodManager.java
+++ FoodManager.java
@@ -47,3 +47,67 @@ class Food {
         return machineId;
     }
 }
+
+class Buffer {
+
+    private Food[] buffer;
+    private int front = 0, back = 0;
+    public int item_count = 0;
+
+    Buffer(int size) {
+        buffer = new Food[size];
+    }
+
+    public synchronized void put(Food food) {
+
+        while (item_count == buffer.length) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+            }
+        }
+        buffer[back] = food;
+        back = (back + 1) % buffer.length;
+        item_count++;
+        this.notifyAll();
+
+    }
+
+    public synchronized Food get() {
+
+        while (item_count == 0) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+            }
+        }
+
+        Food food = buffer[front];
+        front = (front + 1) % buffer.length;
+        item_count--;
+        this.notifyAll();
+
+        return food;
+    }
+
+    public synchronized char checkType() {
+        if (item_count != 0) {
+            return buffer[front].getType();
+        } else {
+            return 'z';
+        }
+    }
+
+    public synchronized boolean checkEmpty() {
+        return item_count == 0;
+    }
+
+    public void returnQueue() {
+        String result = "";
+
+        for (int i = 0; i < item_count; i++) {
+            result = result + buffer[i].getType() + buffer[i].getId() + " ";
+        }
+        System.out.println(result);
+    }
+}
