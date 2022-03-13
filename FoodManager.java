804913882fc351556a4e81737bbd9a5802bbd12c
machi-a <laijyeyi@gmail.com>
2022-03-13 20:40:40 +0800

    added summary builder

--- FoodManager.java
+++ FoodManager.java
@@ -1,10 +1,10 @@
 import java.io.*;
 import java.security.PublicKey;
+import java.util.*;
 import java.util.concurrent.*;
 import javax.sound.sampled.SourceDataLine;
 import javax.sql.rowset.spi.SyncResolver;
 import java.util.concurrent.TimeUnit;
-import java.util.ArrayList;
 
 public class FoodManager {
     static int numHotdog, numBurger, numSlots, numHotdogMakers, numBurgerMakers, numHotdogPackers, numBurgerPackers;
@@ -90,6 +90,7 @@ public class FoodManager {
         Buffer buffer = new Buffer(numSlots);
         cleanUpFiles(filename);
         createFile(filename);
+        Hashtable<String, Integer> summary = new Hashtable<String, Integer>();
 
         // create runnables for each type of thread
 
@@ -108,6 +109,7 @@ public class FoodManager {
                     writeFile(thread_name + " puts hotdog id:" + Integer.toString(newFood.getId())); // write to logfile
                     buffer.put(newFood);
                     gowork(writeTime); // spend time to send to queue
+                    summary.replace(thread_name, summary.get(thread_name) + 1);
                 }
             }
         };
@@ -127,6 +129,7 @@ public class FoodManager {
                     writeFile(thread_name + " puts burger id:" + Integer.toString(newFood.getId())); // write to logfile
                     buffer.put(newFood);
                     gowork(writeTime); // spend time to send to queue
+                    summary.replace(thread_name, summary.get(thread_name) + 1);
                 }
             }
         };
@@ -151,6 +154,7 @@ public class FoodManager {
                     if (check){
                         gowork(packTime); // time to take to pack
                         writeFile(thread_name + " gets hotdog id:" + Integer.toString(newFood.getId()) + " from " + newFood.getMachineId()); // write to file
+                        summary.replace(thread_name, summary.get(thread_name) + 1);
                         check = false;
                     }
 
@@ -171,6 +175,7 @@ public class FoodManager {
                                 numBurgerPacked++;
                                 System.out.println("bp" + numBurgerPacked);
                                 gowork(writeTime); // time to taken to take from queue
+                                summary.replace(thread_name, summary.get(thread_name) + 1);
                                 check = true;
                             }
                         }
@@ -192,34 +197,61 @@ public class FoodManager {
         for (int i = 0; i < HotdogMakerThreads.length; i++) {
             String name = "hm";
             HotdogMakerThreads[i] = new Thread(HotdogMakerRunnable, name.concat(Integer.toString(i)));
+            summary.put(name.concat(Integer.toString(i)), 0);
             HotdogMakerThreads[i].start();
         }
         Thread[] BurgerMakerThreads = new Thread[numBurgerMakers];
         for (int i = 0; i < BurgerMakerThreads.length; i++) {
             String name = "bm";
             BurgerMakerThreads[i] = new Thread(BurgerMakerRunnable, name.concat(Integer.toString(i)));
+            summary.put(name.concat(Integer.toString(i)), 0);
             BurgerMakerThreads[i].start();
         }
         Thread[] HotdogPackerThreads = new Thread[numHotdogPackers];
         for (int i = 0; i < HotdogPackerThreads.length; i++) {
             String name = "hp";
             HotdogPackerThreads[i] = new Thread(HotdogPackerRunnable, name.concat(Integer.toString(i)));
+            summary.put(name.concat(Integer.toString(i)), 0);
             HotdogPackerThreads[i].start();
         }
         Thread[] BurgerPackerThreads = new Thread[numBurgerPackers];
         for (int i = 0; i < BurgerPackerThreads.length; i++) {
             String name = "bp";
             BurgerPackerThreads[i] = new Thread(BurgerPackerRunnable, name.concat(Integer.toString(i)));
+            summary.put(name.concat(Integer.toString(i)), 0);
             BurgerPackerThreads[i].start();
         }
-
+        System.out.println(summary);
         try {
             Thread.sleep(2000);
             while(Thread.activeCount() != 1);
-            buffer.returnQueue();
 
+            TreeMap<String, Integer> test = new TreeMap<String, Integer>(summary);
+
+            Set<String> keys = test.keySet();
+            Iterator<String> itr = keys.iterator();
+            while (itr.hasNext()){
+                String s = itr.next();
+                if (s.charAt(1) == 'p'){
+                    System.out.println(s + " packs " + test.get(s));
+                } else if (s.charAt(1) == 'm'){
+                    System.out.println(s + " makes " + test.get(s));
+                }
+            }
+
+//            Enumeration<String> e = test.keys();
+//            while  (e.hasMoreElements()){
+//                String key = e.nextElement();
+//                if (key.charAt(1) == 'p'){
+//                    System.out.println(key + " packs " + summary.get(key));
+//                } else if (key.charAt(1) == 'm'){
+//                    System.out.println(key + " makes " + summary.get(key));
+//                }
+//            }
 
         } catch (InterruptedException e) { e.printStackTrace();}
+
+
     }
 }
 
