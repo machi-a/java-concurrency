91468e78119d1e4bc66b80b24e99e9742407e193
machi-a <laijyeyi@gmail.com>
2022-03-13 16:30:16 +0800

    print parameters + comments

--- FoodManager.java
+++ FoodManager.java
@@ -4,14 +4,22 @@ public class FoodManager {
     public static void main(String[] args) {
         
         // store parameters
-        numHotdog = Integer.parseInt(args[0]);
-        numBurger = Integer.parseInt(args[1]);
-        numSlots = Integer.parseInt(args[2]);
-        numHotdogMakers = Integer.parseInt(args[3]);
-        numBurgerMakers = Integer.parseInt(args[4]);
-        numHotdogPackers = Integer.parseInt(args[5]);
-        numBurgerPackers = Integer.parseInt(args[6]);
+        numHotdog = Integer.parseInt(args[0]); // number of hotdogs to produce and pack
+        numBurger = Integer.parseInt(args[1]); // number of burgers to produce and pack
+        numSlots = Integer.parseInt(args[2]); // number of slots in the packing queue
+        numHotdogMakers = Integer.parseInt(args[3]); // number of hotdog makers
+        numBurgerMakers = Integer.parseInt(args[4]); // number of burger makers
+        numHotdogPackers = Integer.parseInt(args[5]); // number of hotdog packers
+        numBurgerPackers = Integer.parseInt(args[6]); // numebr of burger packers
 
+        // print parameters
+        System.out.println("hotdogs: " + numHotdog);
+        System.out.println("burgers: " + numBurger);
+        System.out.println("capacity: " + numSlots);
+        System.out.println("hotdog makers: " + numHotdogMakers);
+        System.out.println("burger makers: " + numBurgerMakers);
+        System.out.println("hotdog packers: " + numHotdogPackers);
+        System.out.println("burger packers: " + numBurgerPackers);
         
 
     }
