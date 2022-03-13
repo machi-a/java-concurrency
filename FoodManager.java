e6c616df4fda905a5f4fcf9cba478e638c2df559
machi-a <laijyeyi@gmail.com>
2022-03-13 16:28:34 +0800

    stored parameters

---
+++ FoodManager.java
@@ -0,0 +1,18 @@
+public class FoodManager {
+    static int numHotdog, numBurger, numSlots, numHotdogMakers, numBurgerMakers, numHotdogPackers, numBurgerPackers;
+
+    public static void main(String[] args) {
+        
+        // store parameters
+        numHotdog = Integer.parseInt(args[0]);
+        numBurger = Integer.parseInt(args[1]);
+        numSlots = Integer.parseInt(args[2]);
+        numHotdogMakers = Integer.parseInt(args[3]);
+        numBurgerMakers = Integer.parseInt(args[4]);
+        numHotdogPackers = Integer.parseInt(args[5]);
+        numBurgerPackers = Integer.parseInt(args[6]);
+
+        
+
+    }
+}
